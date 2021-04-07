1c0ae8f839f963b4a5522a50b7af6711c74adabf
Dhiullian <dhiullian@donuz.co>
2021-04-07 09:07:23 -0300

    :rocket: lesson: Objetos de Domínio 10:39, creating vendas context.

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,39 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido : Entity, IAggregateRoot
+    {
+        public int Codigo { get; private set; }
+        public Guid ClientId { get; private set; }
+        public Guid? VoucherId { get; set; }
+        public bool VoucherUtilizado { get; set; }
+        public decimal Desconto { get; private set; }
+        public decimal ValorTotal { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public PedidoStatus PedidoStatus { get; private set; }
+
+        private readonly List<PedidoItem> _pedidoItems;
+        public IReadOnlyCollection<PedidoItem> PedidoItems => _pedidoItems;
+
+        //ref rel. entity
+        public Voucher Voucher { get; private set; }
+
+        public Pedido (Guid clientId, bool voucherUtilizado, decimal desconto, decimal valorTotal)
+        {
+            ClientId = clientId;
+            VoucherUtilizado = voucherUtilizado;
+            Desconto = desconto;
+            ValorTotal = valorTotal;
+            _pedidoItems = new List<PedidoItem>();
+        }
+
+        protected Pedido()
+        {
+            _pedidoItems = new List<PedidoItem>();
+        }
+    }
+}
